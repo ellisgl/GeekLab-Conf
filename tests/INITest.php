0dd23e6e16466ece3417a9065e281a67e50138e9
ellisgl <xelliss@gmail.com>
2018-11-23 18:55:17 -0600

    Renaming stuff. Less typing = good for wrists.

--- tests/INITest.php
+++ tests/INITest.php
@@ -6,7 +6,7 @@ use PHPUnit\Framework\TestCase;
 class ConfINI extends TestCase
 {
     /**
-     * @var Configuration\INI $configuration
+     * @var Conf\INI $configuration
      */
     protected static $configuration;
 
@@ -24,7 +24,7 @@ class ConfINI extends TestCase
         $configurationDirectory = __DIR__ . DS . 'data' . DS . 'ini' . DS . 'configurations' . DS;
 
         // Let's get loaded.
-        self::$configuration = new GeekLab\Conf\INI($systemFile, $configurationDirectory);
+        self::$configuration = new Conf\INI($systemFile, $configurationDirectory);
         self::$configuration->load();
 
         // Sometimes you just want to see it.
@@ -40,55 +40,55 @@ class ConfINI extends TestCase
     /** @test */
     public function testThatItImplementsConfigurationInterface()
     {
-        $this->assertInstanceOf('GeekLab\Configuration\ConfigurationInterface', self::$configuration, 'Configuration\INI does not implement Configuration\ConfigurationInterface!');
+        $this->assertInstanceOf('GeekLab\Conf\ConfInterface', self::$configuration, 'Conf\INI does not implement Conf\ConfnInterface!');
     }
 
     /** @test */
     public function testThatItExtendsConfigurationAbstract()
     {
-        $this->assertInstanceOf('GeekLab\Configuration\ConfigurationAbstract', self::$configuration, 'Configuration\INI does not an instance of Configuration\ConfigurationAbstract!');
+        $this->assertInstanceOf('GeekLab\Conf\ConfAbstract', self::$configuration, 'Conf\INI does not an instance of Conf\ConfAbstract!');
     }
 
     /** @test */
     public function testGetAllReturnsArray()
     {
         // Get compiled config. Is it an array?
-        $this->assertTrue(is_array(self::$configuration->getAll()), 'Configuration\INI->getAll() did not return an array!');
+        $this->assertTrue(is_array(self::$configuration->getAll()), 'Conf\INI->getAll() did not return an array!');
     }
 
     /** @test */
     public function testGetBasic()
     {
         // Basic get.
-        $this->assertEquals('CrazyWebApp', self::$configuration->get('SERVICE'), 'Configuration\INI->get() "Basic" failed!');
+        $this->assertEquals('CrazyWebApp', self::$configuration->get('SERVICE'), 'Conf\INI->get() "Basic" failed!');
     }
 
     /** @test */
     public function testGetIsCaseInsensitive()
     {
         // Make sure it case doesn't matter.
-        $this->assertEquals('CrazyWebApp', self::$configuration->get('SeRvIcE'), 'Configuration\INI->get() case change failed!');
+        $this->assertEquals('CrazyWebApp', self::$configuration->get('SeRvIcE'), 'Conf\INI->get() case change failed!');
     }
 
     /** @test */
     public function testGetWorksWithDotNotation()
     {
         // Test dot notation.
-        $this->assertEquals('localhost', self::$configuration->get('database.host'), 'Configuration\INI->get() dot notation failed!');
+        $this->assertEquals('localhost', self::$configuration->get('database.host'), 'Conf\INI->get() dot notation failed!');
     }
 
     /** @test */
     public function testThatSpacesWhereConvertedToUnderScores()
     {
         // Test spaces to underscore.
-        $this->assertEquals('space pants', self::$configuration->get('space_pants.look_at_my'), 'Configuration\INI did not change spaces in keys to underscores!');
+        $this->assertEquals('space pants', self::$configuration->get('space_pants.look_at_my'), 'Conf\INI did not change spaces in keys to underscores!');
     }
 
     /** @test */
     public function testThatPeriodsWhereConvertedToUnderScores()
     {
         // Test periods to underscore.
-        $this->assertEquals('And that is a fact!', self::$configuration->get('other_stuff._i_like_dots_period'), 'Configuration\INI) did not change periods in keys to underscores!');
+        $this->assertEquals('And that is a fact!', self::$configuration->get('other_stuff._i_like_dots_period'), 'Conf\INI) did not change periods in keys to underscores!');
     }
 
     /** @test */
@@ -96,68 +96,68 @@ class ConfINI extends TestCase
     {
         // Test getting just an array from key.
         $sArr = array('LOOK_AT_MY' => 'space pants');
-        $this->assertTrue(is_array(self::$configuration->get('space_pants')), 'Configuration\INI->get() did not return an array for a "section"!');
-        $this->assertEquals($sArr, self::$configuration->get('space_pants'), 'Configuration\INI->get() did not return expected array for a "section"!');
+        $this->assertTrue(is_array(self::$configuration->get('space_pants')), 'Conf\INI->get() did not return an array for a "section"!');
+        $this->assertEquals($sArr, self::$configuration->get('space_pants'), 'Conf\INI->get() did not return expected array for a "section"!');
     }
 
     /** @test */
     public function testThatItCanMerge()
     {
         // Test the merging.
-        $this->assertEquals('something', self::$configuration->get('devstuff.x'), 'Configuration\INI did not properly merge values!');
-        $this->assertEquals('ellisgl', self::$configuration->get('database.user'), 'Configuration\INI did not properly merge values!');
+        $this->assertEquals('something', self::$configuration->get('devstuff.x'), 'Conf\INI did not properly merge values!');
+        $this->assertEquals('ellisgl', self::$configuration->get('database.user'), 'Conf\INI did not properly merge values!');
     }
 
     /** @test */
     public function testThatItCanReplaceSelfReferencedPlaceholders()
     {
         // Test the self referenced placeholder replacement.
-        $this->assertEquals('mysql:host=localhost;dbname=ellisgldb', self::$configuration->get('database.dsn'), 'Configuration\INI did not replace self referenced placeholders!');
+        $this->assertEquals('mysql:host=localhost;dbname=ellisgldb', self::$configuration->get('database.dsn'), 'Conf\INI did not replace self referenced placeholders!');
     }
 
     /** @test */
     public function testThatItDoesntReplaceMissingSelfReferencedPlaceholders()
     {
         // Make sure we do not replace things we can't reference.
-        $this->assertEquals('@[doesnotexist]', self::$configuration->get('somestuff.d'), 'Configuration\INI replaced a non existing self reference.');
+        $this->assertEquals('@[doesnotexist]', self::$configuration->get('somestuff.d'), 'Conf\INI replaced a non existing self reference.');
     }
 
     /** @test */
     public function testThatItCanReplaceRecursiveSelfReferencedPlaceholders()
     {
         // Test the recursive self referenced placeholder replacement.
-        $this->assertEquals('We Can Do That!', self::$configuration->get('selfreferencedplaceholder.a'), 'Configuration\INI did not replace the recursive self referenced placeholder!');
-        $this->assertEquals('And this!', self::$configuration->get('selfreferencedplaceholder.b'), 'Configuration\INI did not replace the recursive self referenced placeholder!');
-        $this->assertEquals('This too!', self::$configuration->get('selfreferencedplaceholder.c'), 'Configuration\INI did not replace the recursive self referenced placeholder!');
+        $this->assertEquals('We Can Do That!', self::$configuration->get('selfreferencedplaceholder.a'), 'Conf\INI did not replace the recursive self referenced placeholder!');
+        $this->assertEquals('And this!', self::$configuration->get('selfreferencedplaceholder.b'), 'Conf\INI did not replace the recursive self referenced placeholder!');
+        $this->assertEquals('This too!', self::$configuration->get('selfreferencedplaceholder.c'), 'Conf\INI did not replace the recursive self referenced placeholder!');
     }
 
     /** @test */
     public function testThatItDoesntReplaceMissingRecursiveSelfReferencedPlaceholders()
     {
         // Make sure we do not replace things we can't reference.
-        $this->assertEquals('@[doesnt].@[exist]', self::$configuration->get('somestuff.e'), 'Configuration\INI replaced a non existing recursive self reference.');
-        $this->assertEquals('@[doesnt.@[exist]]', self::$configuration->get('somestuff.f'), 'Configuration\INI replaced a non existing recursive self reference.');
-        $this->assertEquals('@[@[doesnt].exist]', self::$configuration->get('somestuff.g'), 'Configuration\INI replaced a non existing recursive self reference.');
-        $this->assertEquals('@[@[doesnt].@[exist]]', self::$configuration->get('somestuff.h'), 'Configuration\INI replaced a non existing recursive self reference.');
+        $this->assertEquals('@[doesnt].@[exist]', self::$configuration->get('somestuff.e'), 'Conf\INI replaced a non existing recursive self reference.');
+        $this->assertEquals('@[doesnt.@[exist]]', self::$configuration->get('somestuff.f'), 'Conf\INI replaced a non existing recursive self reference.');
+        $this->assertEquals('@[@[doesnt].exist]', self::$configuration->get('somestuff.g'), 'Conf\INI replaced a non existing recursive self reference.');
+        $this->assertEquals('@[@[doesnt].@[exist]]', self::$configuration->get('somestuff.h'), 'Conf\INI replaced a non existing recursive self reference.');
     }
 
     /** @test */
     public function testThatItCanReplaceEnvironmentVariablePlaceholders()
     {
         // Test environment variable replacement.
-        $this->assertEquals('utf8', self::$configuration->get('database.charset'), 'Configuration\INI did not replace an environment variable placeholder.');
+        $this->assertEquals('utf8', self::$configuration->get('database.charset'), 'Conf\INI did not replace an environment variable placeholder.');
     }
 
     /** @test */
     public function testThatItCanReplaceEnvironmentVariablePlaceholderWithReferencedPlaceholders()
     {
         // Check to see if we can replace a environment variable placeholder that uses a self referenced placeholder inside of it.
-        $this->assertEquals('utf8', self::$configuration->get('somestuff.k'), 'Configuration\INI did not replace an environment variable placeholder with a self referenced placeholder inside it.');
+        $this->assertEquals('utf8', self::$configuration->get('somestuff.k'), 'Conf\INI did not replace an environment variable placeholder with a self referenced placeholder inside it.');
     }
 
     /** @test */
     public function testThatItDoesntReplaceMissingEnvironmentVariablePlaceholders()
     {
-        $this->assertEquals('$[DOESNOTEXIST]', self::$configuration->get('somestuff.i'), 'Configuration\INI replaced a non existing recursive self reference.');
+        $this->assertEquals('$[DOESNOTEXIST]', self::$configuration->get('somestuff.i'), 'Conf\INI replaced a non existing recursive self reference.');
     }
 }
